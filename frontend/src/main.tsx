379201066abbf0c9f79c600bb3f5a96ec29bef49
CHRISO1503 <christopher_oakes@hotmail.com>
2023-02-11 15:54:18 +0000

    Initialised proxy server

---
+++ frontend/src/main.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./home";
+import Profile from "./profile";
+
+const router = createBrowserRouter([
+    { path: "/", element: <Home /> },
+    { path: "/profile", element: <Profile /> },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
