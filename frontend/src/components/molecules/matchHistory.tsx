285dee2b1c29f146c9ced38f6cd0c1052e7ba30b
CHRISO1503 <christopher_oakes@hotmail.com>
2023-03-18 23:11:21 +0000

    Styled match history page

--- frontend/src/components/molecules/matchHistory.tsx
+++ frontend/src/components/molecules/matchHistory.tsx
@@ -3,13 +3,38 @@ import "./matchHistory.css";
 import TeamImages from "../atoms/teamImages";
 
 export default function matchHistory({ matchHistory }: { matchHistory: any }) {
+    const placements = ["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th"];
+    function matchPosition(placement: number) {
+        if (placement == 1) return "top";
+        else if (placement < 5) {
+            return "win";
+        } else {
+            return "loss";
+        }
+    }
+
     return (
         <div className="matchHistory">
             {matchHistory ? (
                 matchHistory.map((match: any, i: number) => (
-                    <div className="match" key={i}>
+                    <div
+                        className={`match ${matchPosition(
+                            match.info.participants[match.info.playerNumber]
+                                .placement
+                        )}`}
+                        key={i}
+                    >
                         <div className="date">
                             {getDateFromUnix(match.info.game_datetime).date}
+                        </div>
+                        <div className="placement">
+                            {
+                                placements[
+                                    match.info.participants[
+                                        match.info.playerNumber
+                                    ].placement - 1
+                                ]
+                            }
                         </div>
                         <div className="time">
                             {getDateFromUnix(match.info.game_datetime).time}
